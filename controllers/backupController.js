afaea5a652237764d775fcd58bc5375bfaf0b610
Erwin Konink <erwinkonink@gmail.com>
2014-08-23 21:34:40 +0200

    Refactored zipFolder

--- controllers/backupController.js
+++ controllers/backupController.js
@@ -4,6 +4,31 @@ var config = require('app-config');
 var storageConfig = config.azure.storageAccount;
 var sourceConfig = config.dataSource;
 
+function zipFolder(sourceFolder, destinationFolder, callback) {
+	var zip = require('adm-zip');
+	var moment = require('moment');
+	
+	var blobName = moment().format('YYYYMMDD-HHmmss') + '.zip';
+	var backupFilePath = destinationFolder + '/' + blobName;
+
+	try {
+		var zipper = new zip();
+		zipper.addLocalFolder(sourceFolder, destinationFolder);
+		zipper.writeZip(backupFilePath);
+	} catch (ex) {
+		callback(ex);
+		return;
+	}
+
+	var result = {
+		blobName: blobName,
+		backupFilePath: backupFilePath,
+		destinationFolder: destinationFolder
+	};
+
+	callback(null, result);
+};
+
 var list = function(req, res, next) {
 };
 
@@ -11,26 +36,29 @@ var get = function(req, res, next) {
 };
 
 var create = function(req, res, next) {
-	var zip = require('adm-zip');
 	var moment = require('moment');
-	
-	var blobName = moment().format('YYYYMMDD-HHmmss') + '.zip';
-	var tmpFolder = './temp';
 
-	var backupFilePath = tmpFolder + '/' + blobName;
-	var zipper = new zip();
-	zipper.addLocalFolder(sourceConfig.folder, backupFilePath);
-	zipper.writeZip(backupFilePath);
-
-	var client = new storageClient(storageConfig.name, storageConfig.key);
-	client.addBlob(storageConfig.container, blobName, tmpFolder, function(error, result, response) {
+	var addBlobCallbackHandler = function(error, addBlobResult, response) {
 		if (error){
 			res.send(error);
 			return;
 		}
 
-		res.send(sourceConfig.folder + ' saved to ' + blobName);
-	});
+		res.send(addBlobResult);
+	};
+
+	var zipFolderCallbackHandler = function(error, zipResult) {
+		if (error) {
+			res.send(error);
+			return;
+		}
+
+		var client = new storageClient(storageConfig.name, storageConfig.key);
+
+		client.addBlob(storageConfig.container, zipResult.blobName, zipResult.destinationFolder, addBlobCallbackHandler);
+	};
+
+	zipFolder(sourceConfig.folder, './temp', zipFolderCallbackHandler);
 };
 
 var get = function(req, res, next) {
